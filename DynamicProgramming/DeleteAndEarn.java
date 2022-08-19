27c61c1130b2b5512b1e0a3fe2ddaf3f02a0b03a
ashwini-mhetre <108541888+ashwini-mhetre@users.noreply.github.com>
2022-08-19 18:08:30 -0400

    Update DeleteAndEarn.java

--- DynamicProgramming/DeleteAndEarn.java
+++ DynamicProgramming/DeleteAndEarn.java
@@ -20,3 +20,27 @@ class Solution {
         return dp.get(max);
     }
 }
+
+
+
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        Map<Integer, Integer> allPoints = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int val = allPoints.getOrDefault(nums[i], 0);
+            allPoints.put(nums[i], val+nums[i]);
+            max = Math.max(nums[i], max);
+        }
+
+        int[] result = new int[max+1];
+        result[0] = 0;
+        result[1] = allPoints.getOrDefault(1, 0);
+        for(int i=2;i<=max;i++){
+            int gain = allPoints.getOrDefault(i, 0);
+            result[i] = Math.max(result[i-2]+gain, result[i-1]);
+        }
+        return result[max];
+    }
+
+}
